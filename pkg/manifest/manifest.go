9478bd145b4042ebb5432155a28c65b00754e324
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-11-18 10:39:12 -0800

    feat: define simple prod/dev dependency manifest

--- pkg/manifest/manifest.go
+++ pkg/manifest/manifest.go
@@ -1,41 +1,69 @@
 package manifest
 
-type Manifest struct {
-	Dependencies    map[string]Spec `json:"dependencies,omitempty"`
-	DevDependencies map[string]Spec `json:"devDependencies,omitempty"`
-}
+/* -------------------------------------------------------------------------- */
+/*                             Struct: Dependency                             */
+/* -------------------------------------------------------------------------- */
 
+// Dependency is a named specification of an addon. The name should match the
+// actual name of the addon as set by the addon.
 type Dependency struct {
 	Name string
 	Spec Spec
 }
 
+/* -------------------------------------------------------------------------- */
+/*                              Struct: Manifest                              */
+/* -------------------------------------------------------------------------- */
+
+// Manifest is a mapping from environment/target to the list of direct
+// dependencies.
+type Manifest struct {
+	Dependencies    map[string]Spec `json:"dependencies,omitempty"`
+	DevDependencies map[string]Spec `json:"devDependencies,omitempty"`
+}
+
+/* ------------------------------- Method: Add ------------------------------ */
+
+// Add records the provided named specification as a production dependency in
+// the 'Manifest'. If 'name' already exists, then the 'Spec' will be overwritten.
 func (m *Manifest) Add(name string, spec Spec) error {
-	if m.Dependencies == nil {
-		m.Dependencies = make(map[string]Spec)
-	}
-
-	m.Dependencies[name] = spec
+	delete(m.DevDependencies, name)
+	m.set(&m.Dependencies, name, spec)
 
 	return nil
 }
 
+/* ----------------------------- Method: AddDev ----------------------------- */
+
+// AddDev records the provided named specification as a development-only
+// dependency in the 'Manifest'. If 'name' already exists in either production
+// or development dependencies, then the 'Spec' will be overwritten.
 func (m *Manifest) AddDev(name string, spec Spec) error {
-	if m.DevDependencies == nil {
-		m.DevDependencies = make(map[string]Spec)
-	}
-
-	m.DevDependencies[name] = spec
+	delete(m.Dependencies, name)
+	m.set(&m.DevDependencies, name, spec)
 
 	return nil
 }
 
-func (m *Manifest) List() []Dependency {
-	out := make([]Dependency, 0, len(m.Dependencies))
+/* ----------------------------- Method: Remove ----------------------------- */
 
-	for name, s := range m.Dependencies {
-		out = append(out, Dependency{Name: name, Spec: s})
+// Remove deletes the specified addon as a production dependency in the
+// 'Manifest'. This method is a no-op if the addon does not exist.
+func (m *Manifest) Remove(name string) error {
+	delete(m.Dependencies, name)
+	delete(m.DevDependencies, name)
+
+	return nil
+}
+
+/* ------------------------------- Method: set ------------------------------ */
+
+// set adds or overwrites the named addon specification in the provided 'map',
+// first ensuring the 'map' is non-'nil'.
+func (m *Manifest) set(d *map[string]Spec, name string, spec Spec) {
+	if d == nil {
+		*d = make(map[string]Spec)
 	}
 
-	return out
+	(*d)[name] = spec
 }
--- pkg/manifest/spec.go
+++ pkg/manifest/spec.go
@@ -1,22 +1,23 @@
 package manifest
 
+// Spec is a specification of a Godot addon, describing how to access the addon
+// and, implicitly, which version to use.
 type Spec struct {
 	Version string `json:"version,omitempty"`
 
-	// Asset-related fields
-	*Asset
-
-	// Git-related fields
-	*Git
-
-	// Local-related fields
-	*Local
+	*Asset // Embed asset-related fields
+	*Git   // Embed git-related fields
+	*Local // Embed local-related fields
 }
 
+// Asset specifies a Godot addon by the ID on the asset store.
 type Asset struct {
 	Asset uint `json:"asset,omitempty"`
 }
 
+// Git specifies a Godot addon by a URI to a git repository. An additional
+// constraint can be added to specify the version. If no constraints are
+// specified then the main branch will be used.
 type Git struct {
 	Git string `json:"git,omitempty"`
 
@@ -25,6 +26,7 @@ type Git struct {
 	Rev    string `json:"rev,omitempty"`
 }
 
+// Local specifies a path to a locally-available addon.
 type Local struct {
 	Path string `json:"path,omitempty"`
 }
