8d409b033dd09d19b10adb792cdec93f18d6ba1d
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-11-19 11:42:04 -0800

    fix: ensure revision is checked out

--- internal/git/git.go
+++ internal/git/git.go
@@ -10,8 +10,13 @@ import (
 	"github.com/coffeebeats/gdpack/pkg/manifest"
 )
 
-// ErrMissingGit is returned when 'gdpack' cannot find a 'git' executable.
-var ErrMissingGit = errors.New("missing 'git' command")
+var (
+	// ErrMissingGit is returned when 'gdpack' cannot find a 'git' executable.
+	ErrMissingGit = errors.New("missing 'git' command")
+	// ErrMissingRepository is returned when the repository specification is
+	// missing a URI.
+	ErrMissingRepository = errors.New("missing repository")
+)
 
 // Clone downloads a working copy of the specified git repository. Depending on
 // the constraints specified in 'spec' a different type of clone is used:
@@ -20,6 +25,10 @@ var ErrMissingGit = errors.New("missing 'git' command")
 //   - Branch,Tag: The 'HEAD' commit with a depth of '1'.
 //   - None: The 'HEAD' commit on the default branch with a depth of '1'.
 func Clone(ctx context.Context, spec manifest.Git, out string) error {
+	if spec.Git == "" {
+		return ErrMissingRepository
+	}
+
 	git, err := exec.LookPath("git")
 	if err != nil {
 		return err
@@ -40,7 +49,24 @@ func Clone(ctx context.Context, spec manifest.Git, out string) error {
 	args = append(args, spec.Git, out)
 
 	cmd := exec.CommandContext(ctx, git, args...)
+	if err := execute(cmd); err != nil {
+		return err
+	}
 
+	// Check out the specified git ref if required.
+	if spec.Rev != "" {
+		cmd := exec.CommandContext(ctx, git, "checkout", spec.Rev)
+		cmd.Dir = out
+
+		if err := execute(cmd); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func execute(cmd *exec.Cmd) error {
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
 
