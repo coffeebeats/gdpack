53a33e852d383cddb5a0ebc591f3a9cc2dcc6bc7
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-12-03 11:54:30 -0800

    define structure for all subcommands

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,10 +1,9 @@
-/* ----------------------------- Mod: Dependency ---------------------------- */
-
-mod dependency;
-
-pub use dependency::AddArgs;
-
-pub use dependency::handle_add;
+pub mod add;
+pub mod init;
+pub mod install;
+pub mod remove;
+pub mod replace;
+pub mod update;
 
 /* -------------------------------------------------------------------------- */
 /*                               Enum: Commands                               */
@@ -15,6 +14,21 @@ use clap::Subcommand;
 #[derive(Subcommand)]
 pub enum Commands {
     /* ----------------------- Category: Dependencies ----------------------- */
-    /// Add the specified 'addon' as a dependency of a Godot project.
-    Add(AddArgs),
+    /// Add the dependency at the provided 'URI'; can be a filepath or a URL to
+    /// a Git repository.
+    Add(add::Args),
+    /// Remove the specified dependency.
+    Remove(remove::Args),
+    /// Replace a dependency with one at the provided URI; can be a filepath or a URL to a git repository.
+    Replace(replace::Args),
+    /// Update one or more remote addon dependencies to their latest version.
+    Update(update::Args),
+
+    /* --------------------------- Category: Init --------------------------- */
+    /// Create a new `gdpack.toml` manifest for the Godot project.
+    Init(init::Args),
+
+    /* -------------------------- Category: Install ------------------------- */
+    /// Install addon dependencies into the Godot project's addons/ directory.
+    Install(install::Args),
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use clap::Parser;
 use cmd::Commands;
 
 #[derive(Parser)]
-#[command(name = "gvm", author, version, about, verbatim_doc_comment)]
+#[command(name = "gvm", author, version, about)]
 #[command(arg_required_else_help = true)]
 struct Cli {
     #[command(subcommand)]
@@ -25,6 +25,15 @@ fn main() -> Result<()> {
 
     match cli.command {
         /* ----------------------- Category: Dependencies ---------------------- */
-        Commands::Add(args) => cmd::handle_add(args),
+        Commands::Add(args) => cmd::add::handle(args),
+        Commands::Remove(args) => cmd::remove::handle(args),
+        Commands::Replace(args) => cmd::replace::handle(args),
+        Commands::Update(args) => cmd::update::handle(args),
+
+        /* ------------------------- Category: Init ------------------------- */
+        Commands::Init(args) => cmd::init::handle(args),
+
+        /* ------------------------ Category: Install ----------------------- */
+        Commands::Install(args) => cmd::install::handle(args),
     }
 }
