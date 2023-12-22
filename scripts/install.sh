0d52e7164f758c34a5f7a13627417bcb2216940c
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-12-22 13:02:41 -0800

    chore: release v0.0.10

--- scripts/install.sh
+++ scripts/install.sh
@@ -110,7 +110,7 @@ parse_params "$@"
 
 # ------------------------------ Define: Version ----------------------------- #
 
-GDPACK_VERSION="${GDPACK_VERSION=0.0.9}" # x-release-please-version
+GDPACK_VERSION="${GDPACK_VERSION=0.0.10}" # x-release-please-version
 GDPACK_VERSION="v${GDPACK_VERSION#v}"
 
 # ----------------------------- Define: Platform ----------------------------- #
