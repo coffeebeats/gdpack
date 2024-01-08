99a7c1476e15e7e71ec2bfb916797331b36cac72
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-01-07 18:29:59 -0800

    consolidate dependency struct into manifest

--- src/addon/dependency.rs
+++ src/addon/dependency.rs
@@ -1,53 +1,26 @@
-use typed_builder::TypedBuilder;
-
+use crate::config::manifest::Dependency;
+use crate::config::manifest::Source;
 use crate::git;
 
 use super::Addon;
-use super::Spec;
 
 /* -------------------------------------------------------------------------- */
 /*                             Struct: Dependency                             */
 /* -------------------------------------------------------------------------- */
 
-#[derive(Clone, Debug, Eq, PartialEq, TypedBuilder)]
-pub struct Dependency {
-    /// Addon name (used in multi-addon repositories).
-    #[builder(default)]
-    pub name: Option<String>,
-    /// Name of an addon to replace during installs.
-    #[builder(default)]
-    pub replace: Option<String>,
-    /// The source of the addon repository/directory.
-    pub spec: Spec,
-}
-
 /* ---------------------------- Impl: Dependency ---------------------------- */
 
 impl Dependency {
     pub fn install(&self) -> anyhow::Result<super::Addon> {
-        let name = self.name.as_deref();
+        let name = self.addon.as_deref();
 
-        match &self.spec {
-            Spec::Path(p) => Addon::new(p.clone(), name),
-            Spec::Git(s) => {
-                let checkout = git::checkout(s)?;
-                Addon::new(checkout.path, name)
-            }
-            Spec::Release(release) => {
+        match &self.source {
+            Source::Path { path } => Addon::new(&path, name),
+            Source::Git(s) => Addon::new(git::checkout(s)?.path, name),
+            Source::Release(release) => {
                 release.download()?;
                 Addon::new(release.get_path()?, name)
             }
-        }
-    }
-
-    pub fn package(&self) -> Option<String> {
-        match &self.spec {
-            Spec::Release(r) => r.repo.name(),
-            Spec::Git(g) => g.repo.name(),
-            Spec::Path(p) => p
-                .file_name()
-                .and_then(std::ffi::OsStr::to_str)
-                .map(str::to_owned),
         }
     }
 }
--- src/addon/mod.rs
+++ src/addon/mod.rs
@@ -7,23 +7,3 @@ pub use addon::Addon;
 /* ----------------------------- Mod: Dependency ---------------------------- */
 
 mod dependency;
-
-pub use dependency::Dependency;
-
-/* ----------------------------- Mod: Extension ----------------------------- */
-
-mod extension;
-
-pub use extension::Extension;
-
-/* ------------------------------- Mod: Plugin ------------------------------ */
-
-mod plugin;
-
-pub use plugin::Plugin;
-
-/* -------------------------------- Mod: Spec ------------------------------- */
-
-mod spec;
-
-pub use spec::Spec;
