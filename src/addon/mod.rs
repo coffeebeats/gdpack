8112ab4ed55befd7ac01f0259539246af4db6716
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-01-09 22:34:00 -0800

    Define an addon module with common behavior

--- src/addon/mod.rs
+++ src/addon/mod.rs
@@ -3,7 +3,43 @@
 mod addon;
 
 pub use addon::Addon;
+pub use addon::Error as AddonError;
 
-/* ----------------------------- Mod: Dependency ---------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                             Trait: Installable                             */
+/* -------------------------------------------------------------------------- */
 
-mod dependency;
+pub trait Installable {
+    fn install_to(&self, target: impl AsRef<Path>) -> std::io::Result<()>;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                         Function: clone_recursively                        */
+/* -------------------------------------------------------------------------- */
+
+use std::path::Path;
+
+/// Clone files recursively from source to destination using the provided copy
+/// function. See https://nick.groenen.me/notes/recursively-copy-files-in-rust/.
+fn clone_recursively(
+    src: impl AsRef<Path>,
+    dst: impl AsRef<Path>,
+    copy_fn: fn(&Path, &Path) -> std::io::Result<()>,
+) -> std::io::Result<()> {
+    std::fs::create_dir_all(&dst)?;
+
+    for entry in std::fs::read_dir(src)? {
+        let entry = entry?;
+
+        if entry.file_type()?.is_dir() {
+            clone_recursively(entry.path(), dst.as_ref().join(entry.file_name()), copy_fn)?;
+        } else {
+            copy_fn(
+                entry.path().as_ref(),
+                dst.as_ref().join(entry.file_name()).as_ref(),
+            )?;
+        }
+    }
+
+    Ok(())
+}
