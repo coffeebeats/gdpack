367bdce812ca4bf9d04ade5b26f7e0f3b9acb231
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-01-22 15:34:10 -0800

    feat: define manifest methods for accessing script templates

---
+++ src/config/manifest/project.rs
@@ -0,0 +1,106 @@
+use toml_edit::Document;
+use typed_builder::TypedBuilder;
+
+use crate::core::ScriptTemplates;
+
+pub(super) const MANIFEST_SECTION_PROJECT: &str = "project";
+pub(super) const MANIFEST_SECTION_SCRIPT_TEMPLATES: &str = "script_templates";
+
+/* -------------------------------------------------------------------------- */
+/*                               Struct: Project                              */
+/* -------------------------------------------------------------------------- */
+
+/// [`Project`] is used to immutably view the project configuration within the
+/// provided [`toml_edit::Document`].
+#[derive(Debug, TypedBuilder)]
+pub struct Project<'a> {
+    document: &'a Document,
+}
+
+/* ------------------------------ Impl: Project ----------------------------- */
+
+impl<'a> Project<'a> {
+    /// `get_script_templates` returns the script template-related configuration
+    /// within the `project` table of the [`super::Manifest`].
+    pub fn get_script_templates(&self) -> Option<ScriptTemplates> {
+        self.document
+            .get(MANIFEST_SECTION_PROJECT)
+            .and_then(|v| v.as_table_like())
+            .and_then(|t| t.get(MANIFEST_SECTION_SCRIPT_TEMPLATES))
+            .and_then(|v| ScriptTemplates::try_from(v).ok())
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                 Mod: Tests                                 */
+/* -------------------------------------------------------------------------- */
+
+#[cfg(test)]
+mod tests {
+    use serde::de::Deserialize;
+    use std::path::PathBuf;
+    use toml_edit::de::ValueDeserializer;
+
+    use crate::core::ScriptTemplates;
+
+    /* ------------------------ Test: Deserialization ----------------------- */
+
+    macro_rules! test_de_templates {
+        ($name:ident, $input:expr, $want:expr) => {
+            #[test]
+            fn $name() {
+                assert_eq!(
+                    $input
+                        .parse::<ValueDeserializer>()
+                        .and_then(ScriptTemplates::deserialize),
+                    Ok($want),
+                );
+            }
+        };
+    }
+
+    test_de_templates!(
+        test_de_templates_include_succeeds,
+        r#"{ include = ["./a.gd", "../a.gd", "a.gd", "/a.gd"] }"#,
+        ScriptTemplates::builder()
+            .include(
+                vec!["./a.gd", "../a.gd", "a.gd", "/a.gd"]
+                    .into_iter()
+                    .map(PathBuf::from)
+                    .collect::<Vec<_>>()
+            )
+            .build()
+    );
+
+    test_de_templates!(
+        test_de_templates_export_from_inline_table_succeeds,
+        r#"{ export = ["./a.gd", "../a.gd", "a.gd", "/a.gd"] }"#,
+        ScriptTemplates::builder()
+            .export(
+                vec!["./a.gd", "../a.gd", "a.gd", "/a.gd"]
+                    .into_iter()
+                    .map(PathBuf::from)
+                    .collect::<Vec<_>>()
+            )
+            .build()
+    );
+
+    test_de_templates!(
+        test_de_templates_include_and_export_succeeds,
+        r#"{ include = ["./a.gd", "../a.gd", "a.gd", "/a.gd"], export = ["./a.gd", "../a.gd", "a.gd", "/a.gd"] }"#,
+        ScriptTemplates::builder()
+            .include(
+                vec!["./a.gd", "../a.gd", "a.gd", "/a.gd"]
+                    .into_iter()
+                    .map(PathBuf::from)
+                    .collect::<Vec<_>>()
+            )
+            .export(
+                vec!["./a.gd", "../a.gd", "a.gd", "/a.gd"]
+                    .into_iter()
+                    .map(PathBuf::from)
+                    .collect::<Vec<_>>()
+            )
+            .build()
+    );
+}
