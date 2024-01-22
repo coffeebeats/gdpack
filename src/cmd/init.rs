4a83ccc244f13a2909e904533f409ee788449278
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-01-22 15:33:20 -0800

    fix: fail init cmd if manifest already exists

--- src/cmd/init.rs
+++ src/cmd/init.rs
@@ -1,14 +1,18 @@
+use anyhow::anyhow;
 use std::path::PathBuf;
 
 use crate::config::Configuration;
 use crate::config::Manifest;
+use crate::config::Parsable;
 use crate::config::Persistable;
 
 const STR_USAGE: &str = "
+Thanks for using 'gdpack'!
+
 Use `gdpack add` to add plugin dependencies to your project.
 
 For example:
-   gdpack add https://github.com/bitwes/Gut --tag 9.1.1
+   gdpack add https://github.com/bitwes/Gut --tag 9.1.1 -d
 ";
 
 /* -------------------------------------------------------------------------- */
@@ -28,6 +32,14 @@ pub struct Args {
 
 pub fn handle(args: Args) -> anyhow::Result<()> {
     let path_project = super::parse_project(args.project)?;
+
+    let path_manifest = path_project.join(Manifest::file_name().unwrap());
+    if (&path_manifest).is_file() && Manifest::parse_file(&path_manifest).is_ok() {
+        return Err(anyhow!(
+            "manifest already exists: {}",
+            path_manifest.to_str().unwrap()
+        ));
+    }
 
     Manifest::persist(
         &Manifest::default(),
